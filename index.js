dc1cbd6a88786f5c72b84de991fb273791b84ab1
zhenhongswell <zhenhongswell@gmail.com>
2024-02-08 17:57:37 +0800

    -add gameplay controller -check tie if no space to set marks

--- index.js
+++ index.js
@@ -42,10 +42,24 @@ const Tic_Tac_Toe_module = (function(){
             console.log(`board[${row}][${col}] = ${board[row][col]}`);
             console.log(getBoard());
             // checking the game stat after setting the mark!
-            return checkGameStat(player);
+            // return checkGameStat(player,getBoard());
         }
 
-        const checkGameStat = (player)=>{
+        const checkGameStat = (player,board)=>{
+            let checkspaceToSetMark = 0;
+            
+            
+            board.forEach(row_element => {
+                row_element.forEach(col_elemet =>{
+                    if (row_element!== null){//checking if tie = there's no space to put marks
+                        checkspaceToSetMark++;
+                    }
+                })
+            });
+
+            if (checkspaceToSetMark === 9){
+                return "tie!";
+            }
             if (//checking same row
                 board[0][0] === player.playerMark && board[0][1] === player.playerMark && board[0][2] === player.playerMark ||
                 board[1][0] === player.playerMark && board[1][1] === player.playerMark && board[1][2] === player.playerMark || 
@@ -73,6 +87,7 @@ const Tic_Tac_Toe_module = (function(){
         return {
             getBoard,
             setMarkInBoard,
+            checkGameStat,
         }
     }
 
@@ -84,17 +99,54 @@ const Tic_Tac_Toe_module = (function(){
             playerMark,
         }
     }
+
+
+    const gamePlayController = function(){
+        const playerX = player('playerX','X');
+        const playerO = player('playerO','O');
+        const gameBoard = gameboard()
+        const PlayerSetMarkInBoard = (player,row,col) =>{
+            gameBoard.setMarkInBoard(player,row,col);
+            // console.log(gameBoard.checkGameStat(player));
+            // console.log(gameBoard.getBoard());
+            return gameBoard.checkGameStat(player,gameBoard.getBoard());
+        }
+        return {
+            PlayerSetMarkInBoard,
+            playerX,
+            playerO,
+        }
+        
+    }
     
     return {
         gameboard,
         player,
+        gamePlayController,
     }
 })()
 
-const Newplayer_X = Tic_Tac_Toe_module.player('playerX','X');
-const Newplayer_O = Tic_Tac_Toe_module.player('playerO','O');
-const gameboard =  Tic_Tac_Toe_module.gameboard()
-console.log(gameboard.setMarkInBoard(Newplayer_O,0,0));
-console.log(gameboard.setMarkInBoard(Newplayer_O,1,1));
-console.log(gameboard.setMarkInBoard(Newplayer_O,2,2));
-console.log(gameboard.getBoard());
+const DisplayController = (function(){
+
+})()
+
+const gamePlayController = Tic_Tac_Toe_module.gamePlayController();
+
+gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,0,0);
+gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,0,1);
+gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,0,2);
+gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,1,0);
+gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,1,1);
+gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,1,2);
+gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,2,0);
+gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,2,1);
+console.log(gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,2,2));
+// checking tie
+
+// const Newplayer_X = Tic_Tac_Toe_module.player('playerX','X');
+// const Newplayer_O = Tic_Tac_Toe_module.player('playerO','O');
+// const gameboard =  Tic_Tac_Toe_module.gameboard()
+// console.log(gameboard.setMarkInBoard(player,0,0));
+// console.log(gameboard.setMarkInBoard(Newplayer_O,1,1));
+// console.log(gameboard.setMarkInBoard(Newplayer_O,2,2));
+// console.log(gameboard.getBoard());
