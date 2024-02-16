c1d54d0ad2bbf16bb754bd4a07f736294ca7c3b9
zhenhongswell <zhenhongswell@gmail.com>
2024-02-16 09:36:28 +0800

    redo game logic at new branch

--- index.js
+++ index.js
@@ -1,152 +1,159 @@
-// [0,0,0]
-// [0,0,0]
-// [0,0,0]
+function Gameboard(){
+    const rows = 3
+    const columns = 6;
+    const board = [];
 
-// gameboard 
-
-const Tic_Tac_Toe_module = (function(){
-    const gameboard = function(){
-        
-        const col = 3;
-        const row = 3;
-
-        // 
-        let board = emptyBoard();
-
-        function emptyBoard(board){
-            let emptyBoard = [];
-            for (let index = 0; index < col; index++){
-                let board_col = [];
-                for (let index = 0; index < row; index++) {
-                    board_col.push(null);
-                }
-                emptyBoard.push(board_col);
-            }
-            if (board != null || board != undefined){
-                board = emptyBoard;
-            }
-            return emptyBoard;
-        }
-
-        const getBoard = () =>{
-            return board;
-        }
-
-        const setMarkInBoard = (player,row,col)=>{
-            console.log(`${player.playerName} setMark ${player.playerMark}`)
-            console.log(`board[${row}][${col}] = ${board[row][col]}`);
-            if (board[row][col] !== null){
-                console.log('already got a mark at this position')
-            }
-            board[row][col] = player.playerMark;
-            console.log(`board[${row}][${col}] = ${board[row][col]}`);
-            console.log(getBoard());
-            // checking the game stat after setting the mark!
-            // return checkGameStat(player,getBoard());
-        }
-
-        const checkGameStat = (player,board)=>{
-            let checkspaceToSetMark = 0;
-            
-            
-            board.forEach(row_element => {
-                row_element.forEach(col_elemet =>{
-                    if (row_element!== null){//checking if tie = there's no space to put marks
-                        checkspaceToSetMark++;
-                    }
-                })
-            });
-
-            if (checkspaceToSetMark === 9){
-                return "tie!";
-            }
-            if (//checking same row
-                board[0][0] === player.playerMark && board[0][1] === player.playerMark && board[0][2] === player.playerMark ||
-                board[1][0] === player.playerMark && board[1][1] === player.playerMark && board[1][2] === player.playerMark || 
-                board[2][0] === player.playerMark && board[2][1] === player.playerMark && board[2][2] === player.playerMark
-                ){
-                return `${player.playerName} wins!`
-            }
-            if (//checking same col
-                board[0][0] === player.playerMark && board[1][0] === player.playerMark && board[2][0] === player.playerMark ||
-                board[0][1] === player.playerMark && board[1][1] === player.playerMark && board[2][1] === player.playerMark || 
-                board[0][2] === player.playerMark && board[1][2] === player.playerMark && board[2][2] === player.playerMark
-                ){
-                return `${player.playerName} wins!`
-            }
-            if (//checking the crossover
-                board[0][0] === player.playerMark && board[1][1] === player.playerMark && board[2][2] === player.playerMark ||
-                board[2][0] === player.playerMark && board[1][1] === player.playerMark && board[0][2] === player.playerMark
-                ){
-                
-                return `${player.playerName} wins!`
-            }
-            return 'not thing happen';
-        }
-
-        return {
-            getBoard,
-            setMarkInBoard,
-            checkGameStat,
+    // Creating 2D board for putting 
+    for(let i = 0;i < rows;i++){
+        board[i] = [];
+        for(let j = 0;j < columns;j++){
+            board[i].push(Cell());
         }
     }
 
-    const player = function(name,mark){
-        let playerName = name;
-        let playerMark = mark;
-        return {
-            playerName,
-            playerMark,
-        }
-    }
+    const getBoard = () => board;
 
+    const setMarkInBoard = (row,col,player) =>{
+        if (board[row][col].getMark() !== null){
+            console.log (`already have mark: ${board[row][col].getMark()}`);
+            return;
+        }
+        // console.log(player.playerMark);
+        board[row][col].setMark(player);
 
-    const gamePlayController = function(){
-        const playerX = player('playerX','X');
-        const playerO = player('playerO','O');
-        const gameBoard = gameboard()
-        const PlayerSetMarkInBoard = (player,row,col) =>{
-            gameBoard.setMarkInBoard(player,row,col);
-            // console.log(gameBoard.checkGameStat(player));
-            // console.log(gameBoard.getBoard());
-            return gameBoard.checkGameStat(player,gameBoard.getBoard());
-        }
-        return {
-            PlayerSetMarkInBoard,
-            playerX,
-            playerO,
-        }
-        
     }
-    
     return {
-        gameboard,
-        player,
-        gamePlayController,
+        setMarkInBoard,
+        getBoard,
+
     }
-})()
 
-const DisplayController = (function(){
 
-})()
+}
 
-const gamePlayController = Tic_Tac_Toe_module.gamePlayController();
 
-gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,0,0);
-gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,0,1);
-gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,0,2);
-gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,1,0);
-gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,1,1);
-gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,1,2);
-gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,2,0);
-gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,2,1);
-console.log(gamePlayController.PlayerSetMarkInBoard(gamePlayController.playerX,2,2));
-// checking tie
+function Cell(){
+    let mark = null;
+    let player = null;
 
-// const Newplayer_X = Tic_Tac_Toe_module.player('playerX','X');
-// const Newplayer_O = Tic_Tac_Toe_module.player('playerO','O');
-// const gameboard =  Tic_Tac_Toe_module.gameboard()
-// console.log(gameboard.setMarkInBoard(player,0,0));
-// console.log(gameboard.setMarkInBoard(Newplayer_O,1,1));
-// console.log(gameboard.setMarkInBoard(Newplayer_O,2,2));
-// console.log(gameboard.getBoard());
+    const setMark = (player_) =>{
+        player = player_;
+        mark = player_.playerMark;
+    };
+
+    const getMark = () => mark;
+    const getPlayer = () => player;
+
+    return {
+        setMark,
+        getMark,
+        getPlayer,
+    }
+}
+
+
+function Player(name,mark){
+    const playerName = name;
+    const playerMark = mark;
+
+    return {
+        playerName,
+        playerMark,
+    }
+}
+
+
+function GameController(){
+
+    const playerX = Player("PlayerX",'X');
+    const playerO = Player("PlayerO",'O');
+    const gameboard = Gameboard();
+    // default PlayerO first
+    let currentActivePlayer = playerO;
+
+    const getActivePlayer = () => currentActivePlayer;
+
+    const switchCurrentPlayer = () =>{
+        currentActivePlayer = currentActivePlayer === playerO ? playerX : playerO;
+    }
+
+    const printNewRound = () =>{
+        console.log(`${getActivePlayer().playerName}'s turn`);
+    }
+
+    const playRound = (row,col) => {
+        const currentPlayer  = getActivePlayer();
+        gameboard.setMarkInBoard(row,col,currentPlayer);
+        console.log(checkGameStat(currentPlayer));
+        switchCurrentPlayer();
+        printNewRound();
+    }
+
+
+    const checkGameStat = (player) =>{
+        
+        const board = gameboard.getBoard();
+
+        // checking if still have space to put marks 
+        // may not need to check this
+        let CanPutMark = false;
+        board.forEach(each_row => {
+            each_row.forEach(each_col => {
+                if (each_col.getMark() === null){
+                    CanPutMark = true;
+                }
+            });
+        });
+        if (!CanPutMark){
+            return 'have no space to put marks';
+        }
+
+
+
+        if (//checking same row
+        board[0][0].getMark() === player.playerMark && board[0][1].getMark() === player.playerMark && board[0][2].getMark() === player.playerMark ||
+        board[1][0].getMark() === player.playerMark && board[1][1].getMark() === player.playerMark && board[1][2].getMark() === player.playerMark ||
+        board[2][0].getMark() === player.playerMark && board[2][1].getMark() === player.playerMark && board[2][2].getMark() === player.playerMark
+        ) {
+            return `${player.playerName} wins!`
+        }
+        if (//checking same col
+        board[0][0].getMark() === player.playerMark && board[1][0].getMark() === player.playerMark && board[2][0].getMark() === player.playerMark ||
+        board[0][1].getMark() === player.playerMark && board[1][1].getMark() === player.playerMark && board[2][1].getMark() === player.playerMark ||
+        board[0][2].getMark() === player.playerMark && board[1][2].getMark() === player.playerMark && board[2][2].getMark() === player.playerMark
+        ) {
+            return `${player.playerName} wins!`
+        }
+        if (//checking the crossover
+        board[0][0].getMark() === player.playerMark && board[1][1].getMark() === player.playerMark && board[2][2].getMark() === player.playerMark ||
+        board[2][0].getMark() === player.playerMark && board[1][1].getMark() === player.playerMark && board[0][2].getMark() === player.playerMark
+        ) {
+
+            return `${player.playerName} wins!`
+        }
+
+        return 'no thing happened'
+    }
+
+    return {
+        getActivePlayer,
+        playRound,
+    }
+}
+
+const game = GameController();
+
+game.playRound(0,0);
+game.playRound(0,1);
+game.playRound(0,2);
+game.playRound(1,0);
+game.playRound(1,1);
+game.playRound(1,2);
+game.playRound(2,0);
+game.playRound(2,1);
+game.playRound(2,2);
+game.playRound(2,2);
+
+
+
+
